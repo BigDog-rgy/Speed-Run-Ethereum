79c04fd962d2b586b13ec2f224c94a115824bf70
cvrol <cvrolson@gmail.com>
2024-03-13 21:51:58 -0500

    functions: stake, execute, timeLeft, withdraw

--- packages/hardhat/contracts/Staker.sol
+++ packages/hardhat/contracts/Staker.sol
@@ -8,9 +8,59 @@ contract Staker {
 
   ExampleExternalContract public exampleExternalContract;
 
+  mapping (address => uint256) public balances;
+
+  uint256 public constant threshold = 1 ether;
+  uint256 public deadline = block.timestamp + 30 seconds;
+
+  bool public openForWithdraw = false;
+
+  event Stake(address indexed staker, uint256 amount);
+
   constructor(address exampleExternalContractAddress) {
       exampleExternalContract = ExampleExternalContract(exampleExternalContractAddress);
   }
+
+  function stake() public payable {
+    require(msg.value > 0, "Cannot send 0 ETH");
+    balances[msg.sender] += msg.value;
+    emit Stake(msg.sender, msg.value);
+  }
+
+  function execute() public {
+    // check current time is past deadline
+    require(block.timestamp > deadline, "Deadline has not passed yet.");
+
+    if(address(this).balance >= threshold) {
+      exampleExternalContract.complete{value: address(this).balance}();
+    } else {
+      openForWithdraw = true;
+    }
+  }
+
+  function timeLeft() public view returns (uint256) {
+    if (block.timestamp >= deadline) {
+      return 0;
+    } else {
+      return deadline - block.timestamp;
+    }
+  }
+
+  function withdraw() public {
+    require(openForWithdraw, "Withdrawal is not open.");
+    uint256 stakedAmount = balances[msg.sender];
+    require(stakedAmount > 0, "You have no balance to withdraw>");
+
+    // Reset the staker's balance before transfer to prevent re-entrancy attacks
+    balances[msg.sender] = 0;
+    (bool sent, ) = msg.sender.call{value: stakedAmount}("");
+    require(sent, "Failed to send Ether.");
+  }
+
+  receive() external payable {
+    stake();
+  }
+  
 
   // Collect funds in a payable `stake()` function and track individual `balances` with a mapping:
   // (Make sure to add a `Stake(address,uint256)` event and emit it for the frontend `All Stakings` tab to display)
